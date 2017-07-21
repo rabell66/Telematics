adfcc085de085a4662b95fe87c6fe51ffff5f249
rabell66 <raabell66@gmail.com>
2017-07-21 11:01:10 -0500

    Working on updating HTML

--- src/com/theironyard/installparty/Main.java
+++ src/com/theironyard/installparty/Main.java
@@ -1,27 +1,23 @@
 package com.theironyard.installparty;
 
+import com.sun.tools.javadoc.JavaScriptScanner;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-//        double consumption = tanner.nextInt();
-//        double lastOilChange = tanner.nextInt();
-//        double engineSize = tanner.nextInt();
-
         VehicleInfo vehicleInfo = new VehicleInfo();
+        TelematicsService telematicsService = new TelematicsService();
         vehicleInfo.setVIN(( promptForInt("Enter VIN number:")));
-        System.out.println(vehicleInfo.VIN);
-//        vehicleInfo.setConsumption( promptForDouble("How much gas was used?"));
-//        vehicleInfo.getEngineSize(promptForDouble("What is the engine size?"));
-//        vehicleInfo.getLastOilChange(promptForDouble("Odometer reading at last oil change?"));
-//        vehicleInfo.getOdomenter(promptForDouble("Current Odomenter reading?"))
+        vehicleInfo.setOdomenter(promptForDouble("Current Odomenter reading?"));
+        vehicleInfo.setConsumption(promptForDouble("How many gallons were used?"));
+        vehicleInfo.setLastOilChange(promptForDouble("Odometer reading at last oil change?"));
+        vehicleInfo.setEngineSize(promptForDouble("What is the engine size in litres?"));
 
 
-
-
-
+        telematicsService.report(vehicleInfo);
 
 
     }
@@ -33,4 +29,13 @@ public class Main {
         return answer;
 
     }
+
+    private static double promptForDouble(String s){
+        Scanner tanner = new Scanner(System.in);
+        System.out.println(s);
+        double answer = tanner.nextDouble();
+        return answer;
+
+    }
+
 }
--- src/com/theironyard/installparty/TelematicsService.java
+++ src/com/theironyard/installparty/TelematicsService.java
@@ -1,11 +1,56 @@
 package com.theironyard.installparty;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class TelematicsService {
 
-    void report(VehicleInfo vehicleInfo) {
+   public  void report(VehicleInfo vehicleInfo) {
+        // 1. Write vehicleinfo  to a file as json using VIN as file name and a JSON extension
+       String json= "";
+       String fileName = String.valueOf(vehicleInfo.getVIN())+".json";
+       ObjectMapper mapper = new ObjectMapper();
+       try {
+           json = mapper.writeValueAsString(vehicleInfo);
+        }  catch (JsonProcessingException e) {
+           e.printStackTrace();
 
+        }
+        try{
+            File file = new File(fileName);
+
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write(json);
+
+        fileWriter.close();} catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // 2. Find all the files that end with ".json" and convert back to a VehicleInfo object.
+       File file = new File(".");
+       for (File f : file.listFiles()) {
+           if (f.getName().endsWith(".json")) {
+               // Now you have a File object named "f". You can use this in the FileReader constructor
+               // new FileReader(f)
+               try {
+                   FileReader fileReader = new FileReader(f);
+               } catch (FileNotFoundException e) {
+                   e.printStackTrace();
+               }
+           }
+       }
+       try {
+           System.out.println(json);
+           VehicleInfo vi = mapper.readValue(json, VehicleInfo.class);
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+       // 3. Update a dashboard.html
 
 
     }
